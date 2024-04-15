c35eb4ee987999ec67a934eb4bb63b0c75103735
Henrique Poyatos <hpoyatos@yahoo.com.br>
2024-04-15 20:00:18 -0300

    HashSet inicial e exemplo Produto

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import java.util.HashSet;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        HashSetExemplo1();
+    }
+
+    public static void HashSetExemplo1() {
+        HashSet<String> cars = new HashSet<String>();
+        System.out.println(cars.add("Volvo"));
+        System.out.println(cars.add("BMW"));
+        System.out.println(cars.add("Ford"));
+        System.out.println(cars.add("BMW"));
+        System.out.println(cars.add("Mazda"));
+        System.out.println(cars);
     }
 }
---
+++ src/Produto.java
@@ -0,0 +1,37 @@
+public class Produto {
+    private String marca;
+    private String descricao;
+    private float preco;
+    
+    public Produto(String marca, String descricao, float preco) {
+        this.marca = marca;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    
+}
